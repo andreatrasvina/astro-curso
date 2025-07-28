8817513bf7727d7925d15c9b0bfd9b92a336bc0a
andreatrasvina <alucerotrasvina@gmail.com>
2025-07-28 14:55:04 -0700

    amazon buy button

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  env: {
+    schema: {
+      SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' })
+    }
+  }
 });
