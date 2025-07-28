faae7fa27e4b2a9bde0c4f5b72366ad53885d50b
andreatrasvina <alucerotrasvina@gmail.com>
2025-07-28 16:30:25 -0700

    server islands renderiza solo una pequeña parte en el servidor

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig, envField } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  output: 'server',
   integrations: [tailwind()],
   env: {
     schema: {
       SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
-      SCORE_API_ENDPOINT: envField.string({ context: 'client', access: 'public'}),
+      SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public'}),
     }
   }
 });
